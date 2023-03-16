e81c1267894f8fafc723e6e763044698839793cf
I-GALLA <igalla@outlook.fr>
2023-03-16 23:36:26 +0100

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - Entry point
+ * Description: prints a line
+ * Return: Always 1 if Success
+ */
+int main(void)
+{
+	write(2, "and that piece of art is useful - Dora Korpar, 2015-10-19\n", 58);
+	return (1);
+}
