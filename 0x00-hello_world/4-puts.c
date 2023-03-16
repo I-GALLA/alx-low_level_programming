ab7a457c559d889a9dddb104af2330eab6c5fd5c
I-GALLA <igalla@outlook.fr>
2023-03-16 23:07:09 +0100

    5-printf.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,9 @@
 #include <stdio.h>
-
+/*
+ * Description : main - Entry point
+ * 
+ * Return : if success 0
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,5 +1,9 @@
 #include <stdio.h>
-
+/*
+ * Description : main - Entry point
+ * 
+ * Return : if success 0
+ */
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
