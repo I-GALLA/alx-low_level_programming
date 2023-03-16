4ff9727e455d0a60231c7105f62642233c7116dd
I-GALLA <igalla@outlook.fr>
2023-03-16 23:19:01 +0100

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 /**
  * main - Entry point
- * Description: prints exactly Programming is like building a multilingual puzzle
+ * Description: prints a line
  * Return: Always 0 if Success
  */
 int main(void)
