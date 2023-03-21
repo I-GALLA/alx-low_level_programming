34be5e254fb590ff15da11eba10a9265b2ab9d2f
I-GALLA <igalla@outlook.fr>
2023-03-21 22:17:01 +0100

    10-add.c

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -3,7 +3,8 @@
 /**
  * add - Entry point
  * Description: adds two integers and returns the result.
- * @i: and @j: character to be added.
+ * @i: character to be added.
+ * @j: character to be added.
  * Return: the value of the last digit.
  */
 int add(int i, int j)
