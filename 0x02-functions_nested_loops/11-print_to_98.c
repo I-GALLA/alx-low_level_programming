ba1152ebd28899f2d00a3d7d3780c8a8913dd1ee
I-GALLA <igalla@outlook.fr>
2023-03-21 22:19:58 +0100

    11-print_to_98.c

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_to_98 - Entry point
+ * Description: prints all natural numbers from n to 98
+ * @n: character printed to 98
+ * Return: the value of the last digit.
+ */
+void print_to_98(int n)
+{
+	if (n < 98)
+	{
+		for (n = n; n < 98; n++)
+			printf("%d, ", n);
+		printf("%d\n", 98);
+	}
+	else
+	{
+		for (n = n; n > 98; n--)
+			printf("%d, ", n);
+		printf("%d\n", 98);
+	}
+}
