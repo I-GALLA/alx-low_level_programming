ce3103be1bd103d389d92ac2d5087a347d6fba0f
I-GALLA <igalla@outlook.fr>
2023-03-21 21:38:57 +0100

    4-isalpha

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include "main.h"
 /**
- * _islower - Entry point
+ * _isalpha - Entry point
  * Description: checks for lowercase character.
  * @c: character to be checked.
  * Return: 1 if c is lowercase, 0 otherwise
