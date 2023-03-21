fe1e32d478077a6f1a3253701dbe9c1d13014aa9
I-GALLA <igalla@outlook.fr>
2023-03-21 22:16:09 +0100

    10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * add - Entry point
+ * Description: adds two integers and returns the result.
+ * @i: and @j: character to be added.
+ * Return: the value of the last digit.
+ */
+int add(int i, int j)
+{
+	return (i + j);
+}
