169c85f629089f2b562228060ca7ce61163483e1
I-GALLA <igalla@outlook.fr>
2023-03-21 20:53:23 +0100

    1-alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - Entry point
+ * Description: prints the alphabet, in lowercase,
+ * followed by a new line.
+ * Return: 1
+ */
+void print_alphabet(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		_putchar(c);
+	}
+	_putchar('\n');
+}
