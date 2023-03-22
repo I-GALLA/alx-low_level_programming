83d2c5ed5f1add7c9416b7d26fa7cd759d992da9
I-GALLA <igalla@outlook.fr>
2023-03-22 19:58:54 +0100

    7-main.c

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -8,8 +8,10 @@
  */
 int print_last_digit(int n)
 {
+	int last_digit;
 	if (n < 0)
-		n = n * -1;
-	_putchar((n % 10) + '0');
-	return (n % 10);
+	n = -n;
+	last_digit = n % 10;
+	_putchar(last_digit + '0');
+	return (last_digit);
 }
